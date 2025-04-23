511f473ef47bc18c9709eb9c347c5ae7b2c4c822
Bourret Lucas <bourretlucas@gmail.com>
2025-04-23 18:41:32 +0200

    fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743784569|6989876557';
+const CACHE_VERSION = '1745426373|33829070002';
 /** @type {string} */
 const CACHE_PREFIX = 'SpaceMerge-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
